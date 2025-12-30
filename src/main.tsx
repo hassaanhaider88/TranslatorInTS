4894e234377e6cc1213dc40aad005185cbd27990
hassaanhaider88 <hassaanhaider088@gmail.com>
2025-12-30 07:35:40 +0500

    removine extra stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import Del from "./Components/Del";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 
-createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <Del />
-  </Provider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
