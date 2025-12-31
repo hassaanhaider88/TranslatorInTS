979dad60245eeca8b7add78468338bfc9e1cbc3a
hassaanhaider88 <hassaanhaider088@gmail.com>
2025-12-31 11:09:22 +0500

    complete 80%

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { RxCross2 } from "react-icons/rx";
 import { RiTranslateAi } from "react-icons/ri";
 import RightSideBuderIcon from "./Components/RightSideBuderIcon";
+import { toast } from "alert";
 
 const App = () => {
   const languages = [
@@ -18,6 +19,8 @@ const App = () => {
 
   const [lang, setLang] = useState<string>("ur");
   const [SelectText, setSelectText] = useState<string>("");
+  const [TranslatedText, setTranslatedText] = useState("");
+  const [Loading, setLoading] = useState(false);
 
   useEffect(() => {
     console.log(lang);
@@ -26,9 +29,38 @@ const App = () => {
   const hanlePasteCopiedText = () => {
     if (navigator.clipboard) {
       navigator.clipboard.readText().then((text) => {
-        if (!text) return alert("Nothing For Paste");
+        if (!text) return toast("Nothing For Paste");
         setSelectText(text);
       });
+    }
+  };
+  const hanleCopiedText = () => {
+    if (!TranslatedText) return toast("Nothing To Copy");
+
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(TranslatedText);
+      toast("Copied");
+    }
+  };
+
+  const hanleTranslateClick = async () => {
+    if (Loading) return toast("Please wait..");
+    if (SelectText == "") {
+      return toast("Please Enter Text To Translate");
+    } else {
+      setLoading(true);
+      const Res = await fetch(
+        `https://hmk-codeweb-downloads.onrender.com/tranlate?textToTranslate=${SelectText}&Lang=${lang}`
+      );
+      const data = await Res.json();
+      if (data.success) {
+        console.log(data);
+        setTranslatedText(data.data);
+        setLoading(false);
+      } else {
+        toast("Error in Translating Text");
+        setLoading(false);
+      }
     }
   };
 
@@ -85,30 +117,94 @@ const App = () => {
           value={SelectText}
           onChange={(e) => setSelectText(e.target.value)}
           id=""
-          className="w-full bg-gray-300 p-3 h-56 my-3 resize-none rounded-3xl outline-gray-700"
+          className="w-full bg-gray-300 p-3 h-46 my-3 resize-none rounded-3xl outline-gray-700"
         ></textarea>
       </div>
 
-      <div className="LanguagesAndCrossIcon my-5 text-black flex justify-around items-center w-full ">
-        <select
-          className="outline-none cursor-pointer"
-          value={lang}
-          onChange={(e) => setLang(e.target.value)}
-        >
-          {languages.map((l) => (
-            <option key={l.code} value={l.code}>
-              {l.label}
-            </option>
-          ))}
-        </select>
+      <div className="LanguagesAndCrossIcon mt-2 text-black flex justify-around items-center w-full ">
+        <div className="flex gap-4">
+          <span className="text-md font-semibold">Translte To</span>
+          <select
+            className="outline-none cursor-pointer"
+            value={lang}
+            onChange={(e) => setLang(e.target.value)}
+          >
+            {languages.map((l) => (
+              <option key={l.code} value={l.code}>
+                {l.label}
+              </option>
+            ))}
+          </select>
+        </div>
 
-          <button>Translate</button>
-          <BsClipboard
-            onClick={() => hanlePasteCopiedText()}
-            title="Paste The Copied Text"
-            size={24}
-            color="#3750FF"
+        <button
+          onClick={hanleTranslateClick}
+          className={`${
+            Loading ? "cursor-not-allowed" : "cursor-pointer"
+          } relative active:scale-95 duration-300 transition-all text-[13px] rounded-2xl border-0 p-1 bg-[radial-gradient(circle_80px_at_80%_-10%,#ffffff,#181b1b)]`}
+          style={{
+            position: "relative",
+          }}
+        >
+          <div
+            style={{
+              content: '""',
+              position: "absolute",
+              width: "65%",
+              height: "60%",
+              borderRadius: "120px",
+              top: 0,
+              right: 0,
+              boxShadow: "0 0 20px #ffffff38",
+              zIndex: -1,
+            }}
           />
+          <div
+            style={{
+              position: "absolute",
+              width: "70px",
+              height: "100%",
+              borderRadius: "16px",
+              bottom: 0,
+              left: 0,
+              background:
+                "radial-gradient(circle 60px at 0% 100%, #3fe9ff, #0000ff80, transparent)",
+              boxShadow: "-10px 10px 30px #0051ff2d",
+            }}
+          />
+          <div className="relative font-semibold px-6 py-3.5 rounded-[14px] text-white z-10 bg-[radial-gradient(circle_80px_at_80%_-50%,#3750FF,#0f1111)]">
+            <span
+              style={{
+                content: '""',
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+                left: 0,
+                top: 0,
+                borderRadius: "14px",
+                background:
+                  "radial-gradient(circle 60px at 0% 100%, #00e1ff1a, #0000ff11, transparent)",
+                zIndex: 0,
+              }}
+            />
+            {Loading ? "Translating..." : "Translate"}
+          </div>
+        </button>
+
+        <BsClipboard
+          className="cursor-pointer"
+          onClick={() => hanleCopiedText()}
+          title="Paste The Copied Text"
+          size={24}
+          color="#3750FF"
+        />
+      </div>
+      <div className="px-5 w-full md:px-10">
+        <textarea
+          readOnly
+          value={TranslatedText}
+          className="w-full science-gothicURDU text-black font-semibold bg-gray-300 p-3 outline-none h-30 my-3 resize-none rounded-3xl"
+        ></textarea>
       </div>
     </div>
   );
